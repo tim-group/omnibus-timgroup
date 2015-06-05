62fc8c0ee5340a312467ad4934a14630cacb9e8b
Michal Mazurek <michal.mazurek@timgroup.com>
2015-06-05 08:43:04 +0100

    update the stackbuilder project

--- config/projects/stackbuilder.rb
+++ config/projects/stackbuilder.rb
@@ -1,18 +1,20 @@
-name "stackbuilder"
-maintainer "infra@timgroup.com"
-homepage "https://github.com/tim-group"
+name            "stackbuilder"
+maintainer      "infra@timgroup.com"
+homepage        "https://github.com/tim-group"
+description     "stackbuilder"
+replace         "stacks" # XXX
 
-install_dir "/opt/#{name}"
+install_dir     "/opt/#{name}"
 
-build_version Omnibus::BuildVersion.semver
+build_version   ENV['BUILD_NUMBER'] ? ('0.0.' + ENV['BUILD_NUMBER']) : '0.0.0'
 build_iteration 1
 
-override :ruby, version: '1.9.3-p550'
+override        :ruby, version: '1.9.3-p550'
 
-dependency "preparation"
-dependency "version-manifest"
+dependency      "preparation"
+dependency      "version-manifest"
 
-dependency "stackbuilder"
+dependency      "stackbuilder"
 
-exclude "**/.git"
-exclude "**/bundler/git"
+exclude         "**/.git"
+exclude         "**/bundler/git"
