e69df580bd0fb03a37cc616d9cbde5bba7c55118
Michal Mazurek <michal.mazurek@timgroup.com>
2015-06-05 08:15:37 +0100

    update instructions for building omnibus

--- config/projects/omnibus.rb
+++ config/projects/omnibus.rb
@@ -6,7 +6,7 @@ description     "omnibus bundle"
 install_dir     "/opt/#{name}"
 
 build_version   ENV['BUILD_NUMBER'] ? ('0.0.' + ENV['BUILD_NUMBER']) : '0.0.0'
-build_iteration 1
+build_iteration 2
 
 override        :ruby, version: '1.9.3-p550'
 
