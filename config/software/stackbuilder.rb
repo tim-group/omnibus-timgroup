8c0c2680c38546be23fdb8f60a11be8801c78cd6
Michal Mazurek <michal.mazurek@timgroup.com>
2015-06-11 07:20:04 +0100

    bump stackbuilder ruby to 2.1.6 again

--- config/software/stackbuilder.rb
+++ config/software/stackbuilder.rb
@@ -2,9 +2,10 @@ name "stackbuilder"
 default_version "master"
 
 dependency "ruby"
+dependency "bundler"
+
 dependency "ruby-web-test-framework"
 dependency "ruby-nagios-support"
-dependency "bundler"
 
 source git: "http://git/git/github/tim-group/stackbuilder.git"
 
@@ -13,10 +14,10 @@ relative_path "stackbuilder"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
+  gem "install rspec --version '2.13.0' --no-ri --no-rdoc", env: env
   gem "install puppet --version '3.7.5' --no-ri --no-rdoc", env: env
   gem "install mcollective-client --version '2.8.2' --no-ri --no-rdoc", env: env
   gem "install collimator --version '0.0.3' --no-ri --no-rdoc", env: env
-  gem "install rspec --version '2.13.0' --no-ri --no-rdoc", env: env
   gem "install git --version '1.2.9.1' --no-ri --no-rdoc", env: env # XXX orc
 
   command "rake omnibus"
