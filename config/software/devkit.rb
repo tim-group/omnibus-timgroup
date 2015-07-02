5936afdc914f4031734682507e6e7eb16f2c6d97
Michal Mazurek <michal.mazurek@timgroup.com>
2015-07-02 14:40:55 +0100

    update rubocop 0.30.1 -> 0.32.1

--- config/software/devkit.rb
+++ config/software/devkit.rb
@@ -10,14 +10,15 @@ dependency "ruby-nagios-support"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "install rspec --version '2.13.0' --no-ri --no-rdoc", env: env
-  gem "install ci_reporter --version '1.7.3' --no-ri --no-rdoc", env: env
-  gem "install rubocop --version '0.30.1' --no-ri --no-rdoc", env: env
+  gem "install ci_reporter --version '1.7.3' --no-ri --no-rdoc", env: env # XXX newer version available
+  gem "install collimator --version '0.0.3' --no-ri --no-rdoc", env: env # stackbuilder rake sbx:audit_host_machines
+  gem "install fpm --version '1.3.3' --no-ri --no-rdoc", env: env
   gem "install git --version '1.2.9.1' --no-ri --no-rdoc", env: env
   gem "install mcollective-client --version '2.8.2' --no-ri --no-rdoc", env: env
+  gem "install puppet --version '3.7.5' --no-ri --no-rdoc", env: env # XXX newer version available
+  gem "install rspec --version '2.13.0' --no-ri --no-rdoc", env: env # XXX newer version available
+  gem "install rubocop --version '0.32.1' --no-ri --no-rdoc", env: env
   gem "install simplecov --version '0.10.0' --no-ri --no-rdoc", env: env
-  gem "install puppet --version '3.7.5' --no-ri --no-rdoc", env: env
-  gem "install fpm --version '1.3.3' --no-ri --no-rdoc", env: env
 
   # decrease 'rake spec' runtime in stackbuilder by 12 seconds:
   command "sed -i 's/if svrs = IO.select(@listeners, nil, nil, 2.0)/if svrs = IO.select(@listeners, nil, nil, 0.01)/' "\
