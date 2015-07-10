aa99daae7fbbfbcdaa82b1150b229ca2fea93948
Tom Denley <tom.denley@timgroup.com>
2015-07-10 08:13:35 +0100

    tomd: remove unnecessary bits and bump ruby version (thanks for help from mmazurek

--- config/projects/lita.rb
+++ config/projects/lita.rb
@@ -8,10 +8,9 @@ install_dir     "/opt/#{name}"
 build_version   ENV['BUILD_NUMBER'] ? ('0.0.' + ENV['BUILD_NUMBER']) : '0.0.0'
 build_iteration 1
 
-override        :ruby, version: '2.1.6'
+override        :ruby, version: '2.2.1'
 
 dependency      "lita"
-
 
 exclude         "**/.git"
 exclude         "**/bundler/git"
