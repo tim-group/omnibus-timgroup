8518a1caf35fcff42b15b3e1db8c5401afdecddd
Tom Denley <tom.denley@timgroup.com>
2015-07-11 12:02:41 +0100

    tomd: try lita-jenkins plugin gem

--- config/software/lita.rb
+++ config/software/lita.rb
@@ -7,7 +7,7 @@ build do
 
   gem "install lita                --version '4.4.3' --no-ri --no-rdoc", env: env
   gem "install lita-slack          --version '1.5.0' --no-ri --no-rdoc", env: env
-  # gem "install lita-jenkins        --version '0.0.3' --no-ri --no-rdoc", env: env
+  gem "install lita-jenkins        --version '0.0.3' --no-ri --no-rdoc", env: env
   gem "install lita-pagerduty      --version '0.1.0' --no-ri --no-rdoc", env: env
   # gem "install lita-nagios         --version '0.1.2' --no-ri --no-rdoc", env: env
   gem "install lita-sensu          --version '0.2.0' --no-ri --no-rdoc", env: env
