8789278a46db088060a89b76f49601af8aba3060
Adam Gibbins <adam@adamgibbins.com>
2015-08-11 23:15:13 +0100

    Add lita-cron plugin

--- config/software/lita.rb
+++ config/software/lita.rb
@@ -15,4 +15,5 @@ build do
   gem "install lita-directions     --version '1.0.2' --no-ri --no-rdoc", env: env
   gem "install lita-forecast       --version '0.1.7' --no-ri --no-rdoc", env: env
   gem "install lita-boardgamegeek  --version '0.1.1' --no-ri --no-rdoc", env: env
+  gem "install lita-cron           --version '0.0.3' --no-ri --no-rdoc", env: env
 end
