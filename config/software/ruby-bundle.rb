b29bb94885f83490e6f7851eb2174cc81c1cefca
Michal Mazurek <michal.mazurek@timgroup.com>
2015-08-19 09:23:08 +0100

    updated gems in ruby-bundle

--- config/software/ruby-bundle.rb
+++ config/software/ruby-bundle.rb
@@ -1,23 +1,28 @@
-name "ruby-bundle"
+name 'ruby-bundle'
 
-dependency "ruby"
-dependency "bundler"
+dependency 'ruby'
+dependency 'bundler'
 
-dependency "ruby-web-test-framework"
-dependency "ruby-nagios-support"
+dependency 'ruby-web-test-framework'
+dependency 'ruby-nagios-support'
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "install ci_reporter --version '1.7.3' --no-ri --no-rdoc", env: env # XXX newer version available
-  gem "install collimator --version '0.0.3' --no-ri --no-rdoc", env: env # stackbuilder rake sbx:audit_host_machines
-  gem "install fpm --version '1.3.3' --no-ri --no-rdoc", env: env
-  gem "install git --version '1.2.9.1' --no-ri --no-rdoc", env: env
-  gem "install mcollective-client --version '2.8.2' --no-ri --no-rdoc", env: env
-  gem "install puppet --version '3.7.5' --no-ri --no-rdoc", env: env # XXX newer version available
-  gem "install rspec --version '2.13.0' --no-ri --no-rdoc", env: env # XXX newer version available
-  gem "install rubocop --version '0.32.1' --no-ri --no-rdoc", env: env
-  gem "install simplecov --version '0.10.0' --no-ri --no-rdoc", env: env
+  gem 'install ci_reporter --version 2.0.0 --no-ri --no-rdoc', env: env
+  gem 'install ci_reporter_rspec --version 1.0.0 --no-ri --no-rdoc', env: env
+  gem 'install collimator --version 0.0.3 --no-ri --no-rdoc', env: env # stackbuilder
+  gem 'install fpm --version 1.3.3 --no-ri --no-rdoc', env: env
+  gem 'install git --version 1.2.9.1 --no-ri --no-rdoc', env: env
+  gem 'install mcollective-client --version 2.8.2 --no-ri --no-rdoc', env: env
+  gem 'install puppet --version 3.7.5 --no-ri --no-rdoc', env: env # XXX not needed when 'exec' indirector used (sb)
+  gem 'install rspec --version 3.3.0 --no-ri --no-rdoc', env: env
+  gem 'install rubocop --version 0.32.1 --no-ri --no-rdoc', env: env
+  gem 'install simplecov --version 0.10.0 --no-ri --no-rdoc', env: env
+  gem 'install rotp --version 2.1.1 --no-ri --no-rdoc', env: env
+  gem 'install mcollective-test --version 0.4.3 --no-ri --no-rdoc', env: env # dbupdate
+  gem 'install mocha --version 1.1.0 --no-ri --no-rdoc', env: env # dbupdate
+  gem 'install rspec-puppet --version 2.2.0 --no-ri --no-rdoc', env: env # puppet
 
   # decrease 'rake spec' runtime in stackbuilder by 12 seconds:
   command "sed -i 's/if svrs = IO.select(@listeners, nil, nil, 2.0)/if svrs = IO.select(@listeners, nil, nil, 0.01)/' "\
