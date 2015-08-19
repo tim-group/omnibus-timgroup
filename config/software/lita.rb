24dcb59c8b95e9ae1b9c0ef5b79cd1c82072be49
Tom Denley <tom.denley@timgroup.com>
2015-08-19 08:57:53 +0100

    tomd: try building lita using bundler... nfi what I am doing really

--- config/software/lita.rb
+++ config/software/lita.rb
@@ -1,22 +1,16 @@
 name "lita"
+default_version "master"
 
 dependency "ruby"
 dependency "bundler"
+dependency "git"
+
+source git: "http://git/git/lita.git"
+
+relative_path "lita"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  gem "install lita                --version '4.4.3' --no-ri --no-rdoc", env: env
-  gem "install lita-slack          --version '1.5.0' --no-ri --no-rdoc", env: env
-  gem "install lita-jenkins        --version '0.0.3' --no-ri --no-rdoc", env: env
-  gem "install lita-pagerduty      --version '0.1.0' --no-ri --no-rdoc", env: env
-  # gem "install lita-nagios         --version '0.1.2' --no-ri --no-rdoc", env: env
-  gem "install lita-sensu          --version '0.2.0' --no-ri --no-rdoc", env: env
-  gem "install lita-karma          --version '3.0.2' --no-ri --no-rdoc", env: env
-  gem "install lita-directions     --version '1.0.2' --no-ri --no-rdoc", env: env
-  gem "install lita-forecast       --version '0.1.7' --no-ri --no-rdoc", env: env
-  gem "install lita-boardgamegeek  --version '0.1.1' --no-ri --no-rdoc", env: env
-  gem "install lita-ping           --version '1.1.1' --no-ri --no-rdoc", env: env
-
-  system("echo aaaa > #{install_dir}/test")
+  command "bundle install --path #{install_dir}"
 end
